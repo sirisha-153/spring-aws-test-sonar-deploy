71e552781f0ef2805657abb91d8c81d782444202
sirisha-153 <sirimanthena153@gmail.com>
2026-02-28 22:06:30 +0530

    "sonar3"

--- src/test/java/com/example/demo/CseControllertest.java
+++ src/test/java/com/example/demo/CseControllertest.java
@@ -1,18 +1,20 @@
 package com.example.demo;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class CseControllerTest {
-	@Autowired
-	CseController c;
-	@Test
-	void test() {
-		int result = c.addCSE(2, 3);
-		assertEquals(5, result);
-	}
+    
+    @Autowired
+    private CseController c;
 
+    @Test
+    void testAdd() {
+        String result = c.addCSE(2, 3);
+        // Check if result contains the expected text and value
+        assertTrue(result.contains("result added by 23mh1a05i7: 5"));
+    }
 }
